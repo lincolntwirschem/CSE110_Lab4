43762523eead2cb9c8799bb46a413cb2d974ecc8
Tony Nguyen <tonynguyeenn@gmail.com>
2026-01-25 18:43:49 -0800

    Changing module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module edu.ucsd.spendingtracker {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
     opens edu.ucsd.spendingtracker to javafx.fxml;
     exports edu.ucsd.spendingtracker;
